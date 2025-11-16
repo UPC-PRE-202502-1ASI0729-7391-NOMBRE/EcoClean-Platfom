93c03383e76c86c48f988c8b27fdc8cac38103ef
DiegoAndresAvalos <u202313922@upc.edu.pe>
2025-11-16 05:01:40 -0500

    Update UpdateUserRoleRequest.java

---
+++ src/main/java/pe/com/ecocleany/ecosmart/admin/interfaces/rest/dto/UpdateUserRoleRequest.java
@@ -0,0 +1,10 @@
+package pe.com.ecocleany.ecosmart.admin.interfaces.rest.dto;
+
+import jakarta.validation.constraints.NotNull;
+import pe.com.ecocleany.ecosmart.iam.domain.model.RoleName;
+
+public record UpdateUserRoleRequest(
+        @NotNull(message = "El rol es obligatorio")
+        RoleName role
+) {
+}
