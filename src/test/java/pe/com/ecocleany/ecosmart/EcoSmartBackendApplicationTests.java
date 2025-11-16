5e6e62d9b9c8517d645ed22d967ea82a10a8533b
AndS56 <andree567890@gmail.com>
2025-11-16 02:32:35 -0500

    Add new test

---
+++ src/test/java/pe/com/ecocleany/ecosmart/EcoSmartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.ecocleany.ecosmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoSmartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
